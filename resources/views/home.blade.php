69c4de566f92a4e104e6ad0ab6b34a7c909d729f
DESKTOP-BKESKID\kmala <kmalak4822@gmail.com>
2022-10-03 18:25:51 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Strona Główna</title>
+</head>
+
+<body>
+    <h1>DKOWKDWOKDOWKDWODWO ESSA</h1> lubie placki ziemniaczane
+</body>
+
+</html>
