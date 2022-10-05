af88ce1593c30845e45c3a9ce73fbe2f99fe4c6b
DESKTOP-BKESKID\kmala <kmalak4822@gmail.com>
2022-10-05 16:48:29 +0200

    tablica z autami dziala

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class MyController extends Controller
 {
     public static function enterHome()
     {
-        return view("home");
+
+        return view(
+            "home",
+            [
+                "auta" => array(
+                    array("Volvo", 22, 18),
+                    array("BMW", 15, 13),
+                    array("Saab", 5, 2),
+                    array("Land Rover", 17, 15)
+                ),
+            ]
+        );
     }
 }
--- resources/views/home.php
+++ resources/views/home.php
@@ -6,9 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/app.css">
 </head>
 
 <body>
+    <h1>Użytkownicy :</h1>
+    <h2> <?php echo "imie to <a href='/user/1'> {$auta[0][0]} </a>" ?> </h2>
+    <h2> <?php echo "imie to <a href='/user/2'> {$auta[1][0]} </a>" ?> </h2>
+    <h2> <?php echo "imie to <a href='/user/3'> {$auta[2][0]} </a>" ?> </h2>
 </body>
 
 </html>
