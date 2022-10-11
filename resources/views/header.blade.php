1dd818092be7009c09bfd92120dcfc2f9ae15ac6
DESKTOP-BKESKID\kmala <kmalak4822@gmail.com>
2022-10-11 12:27:34 +0200

    small changfes

---
+++ resources/views/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+
+    <div>
+        <div class="topButton"></div>
+        <div class="topButton"></div>
+        <div class="topButton"></div>
+    </div>
+
+    <div>
+        <div class="topButton"></div>
+        <div class="topButton"></div>
+        <div class="topButton"></div>
+    </div>
+
+</header>
--- resources/views/users.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-
-@section('section_2')
-    <h1> Imie to {{ $czlowiek['imie'] }}
-        a wzrost to {{ $czlowiek['wzrost'] }}</h1>
-
-    <h2>Wzrost to {{ $czlowiek['wiek'] }}</h2>
-@endsection
