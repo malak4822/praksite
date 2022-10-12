602391ffadb040c40438c00e7f012c90f72c2850
DESKTOP-BKESKID\kmala <kmalak4822@gmail.com>
2022-10-12 12:34:37 +0200

    not working tryings

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,24 +1,28 @@
-<header class="bg-stone-700 flex justify-end m-auto text-white ">
+<header class="flex auto bg-stone-700 text-white">
+    <div class="flex-auto">
+        <button>
+            <img class="h-16" src="logotype.png" alt="logo"></img>
+        </button>
 
-    <img class="logo mr-auto" src="logotype.png" alt="logo"></img>
+        <button>
+            Search
+        </button>
 
-    <button>
-        Search
-    </button>
+        <button>
+            Ranking
+        </button>
+    </div>
+    <div class="flex-auto">
+        <button>
+            Submit
+        </button>
 
-    <button>
-        Ranking
-    </button>
+        <button>
+            Notification
+        </button>
 
-    <button>
-        Submit
-    </button>
-
-    <button>
-        Notification
-    </button>
-
-    <button>
-        My Page
-    </button>
+        <button>
+            My Page
+        </button>
+    </div>
 </header>
