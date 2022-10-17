6913970e972040f5b679f280151ebb540e603c5e
DESKTOP-BKESKID\kmala <kmalak4822@gmail.com>
2022-10-17 12:54:05 +0200

    done-now-really-first-page

--- resources/views/contentList.blade.php
+++ resources/views/contentList.blade.php
@@ -1,19 +1,25 @@
-    <div class="flex mt-3 aea">
+<div class="">
+    <div class="w-4/5 flex mx-auto mt-3 aea bg-neutral-700 rounded-t-3xl px-5 pt-10">
         <?php
         $user = DB::table('thumbnailContent')->find(1);
         echo '<div class=tile>' . "<img alt=cat1 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-       
+        
         $user = DB::table('thumbnailContent')->find(2);
-         echo '<div class=tile>' . "<img alt=cat2 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-       
+        echo '<div class=tile>' . "<img alt=cat2 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
+        
         $user = DB::table('thumbnailContent')->find(3);
         echo '<div class=tile>' . "<img alt=cat3 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-       
+        
         $user = DB::table('thumbnailContent')->find(4);
         echo '<div class=tile>' . "<img alt=cat4 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-       
+        
         $user = DB::table('thumbnailContent')->find(5);
         echo '<div class=tile>' . "<img alt=cat5 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
         ?>
-
     </div>
+
+    <div class="rounded-b-3xl bg-neutral-700 w-4/5 text-end mx-auto mt-auto">
+        <button>See more</button>
+    </div>
+
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-<div class="flex border-2 m-5">
+<div class="flex m-5">
 
     <div class="texty1 text-center p-10 w-1/2">
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
