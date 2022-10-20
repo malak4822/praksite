b087d67e592428a559c9f6cef50a36660aa7533f
DESKTOP-BKESKID\kmala <kmalak4822@gmail.com>
2022-10-20 17:55:20 +0200

    beter

--- resources/views/contentList.blade.php
+++ resources/views/contentList.blade.php
@@ -1,54 +1,22 @@
-<div>
-    <div class="w-4/5 grid mx-auto aea bg-neutral-700 rounded-t-3xl p-5">
+    <div class="w-4/5 grid mx-auto bg-neutral-700 rounded-t-3xl p-5">
+
         <?php
-        $user = DB::table('thumbnailContent')->find(1);
-        echo '<div class="tile">' . "<img alt=cat1 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
+        $firstfiveuser = DB::table('thumbnailContent')
+            ->whereIn('id', [1, 2, 3, 4, 5])
+            ->get();
         
-        $user = DB::table('thumbnailContent')->find(2);
-        echo '<div class="tile">' . "<img alt=cat2 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-        
-        $user = DB::table('thumbnailContent')->find(3);
-        echo '<div class="tile">' . "<img alt=cat3 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-        
-        $user = DB::table('thumbnailContent')->find(4);
-        echo '<div class="tile">' . "<img alt=cat4 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-        
-        $user = DB::table('thumbnailContent')->find(5);
-        echo '<div class="tile">' . "<img alt=cat5 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
+        $users = DB::table('thumbnailContent')->get();
         ?>
 
-
-
-        </div>
-        <div id=visir class="w-4/5 grid mx-auto aea bg-neutral-700 p-5">
-
-            <?php
-            $user = DB::table('thumbnailContent')->find(6);
-            echo '<div class="tile">' . "<img alt=cat6 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-            
-            $user = DB::table('thumbnailContent')->find(7);
-            echo '<div class="tile">' . "<img alt=cat7 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-            
-            $user = DB::table('thumbnailContent')->find(8);
-            echo '<div class="tile">' . "<img alt=cat8 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-            
-            $user = DB::table('thumbnailContent')->find(9);
-            echo '<div class="tile">' . "<img alt=cat9 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-            
-            $user = DB::table('thumbnailContent')->find(10);
-            echo '<div class="tile">' . "<img alt=cat10 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-            
-            $user = DB::table('thumbnailContent')->find(11);
-            echo '<div class="tile">' . "<img alt=cat11 class=tileinside src=$user->thumbnailURL ></img>" . $user->submitted_by . '<br>' . $user->author . '</div>';
-            ?>
-
-        </div>
+        @foreach ($firstfiveuser as $firi)
+            <div class="text-center bg-neutral-500 p-1 rounded-3xl">
+                <img class='rounded-3xl' src={{ $firi->thumbnailURL }}></img>
+                {{ $firi->author }}
+                {{ $firi->submitted_by }}
+            </div>
+        @endforeach
     </div>
-
-
 
     <div class="rounded-b-3xl  bg-neutral-700 w-4/5 text-end mx-auto">
-        <button onclick="hideContent()" class="bg-white text-black hover:text-white">See more</button>
+        <button onclick="hideContent()" class="bg-white text-black m-5 hover:text-white">See more</button>
     </div>
-
-</div>
